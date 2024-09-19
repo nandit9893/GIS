45ce0825a35fc74436324a1739c9a50b4eb32dd4
Nandit <nanditsharma063@gmail.com>
2024-09-19 12:36:50 +0530

    update

--- front-end/src/Components/MapComponent/MapComponents.jsx
+++ front-end/src/Components/MapComponent/MapComponents.jsx
@@ -3,9 +3,19 @@ import { MapContainer, TileLayer, FeatureGroup } from "react-leaflet";
 import { EditControl } from "react-leaflet-draw";
 import "leaflet/dist/leaflet.css";
 import "leaflet-draw/dist/leaflet.draw.css";
+import L from "leaflet";
 import sendLocation from "../../APIMiddleware/APIMiddleware.js";
 import UpdateMapPosition from "../../Utils/UpdateMapPosition.js";
 import "./MapComponent.css";
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl:
+  "https:cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-icon.png",
+  iconUrl:
+  "https:cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-icon.png",
+  shadowUrl:
+  "https:cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-icon.png",
+});
 
 const MapComponent = ({ state, district }) => {
   const [position, setPosition] = useState([23.5000, 80.7500]);

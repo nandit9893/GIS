ab86da53dc8d8c89424c96991840557e4989a8b2
Nandit <nanditsharma063@gmail.com>
2024-09-19 12:43:09 +0530

    updated map

--- front-end/src/Components/MapComponent/MapComponents.jsx
+++ front-end/src/Components/MapComponent/MapComponents.jsx
@@ -7,19 +7,18 @@ import L from "leaflet";
 import sendLocation from "../../APIMiddleware/APIMiddleware.js";
 import UpdateMapPosition from "../../Utils/UpdateMapPosition.js";
 import "./MapComponent.css";
+
+// Update icon URLs
 delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
-  iconRetinaUrl:
-  "https:cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-icon.png",
-  iconUrl:
-  "https:cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-icon.png",
-  shadowUrl:
-  "https:cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-icon.png",
+  iconRetinaUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
+  iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
+  shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
 });
 
 const MapComponent = ({ state, district }) => {
   const [position, setPosition] = useState([23.5000, 80.7500]);
-  const mapRef = useRef()
+  const mapRef = useRef();
 
   const _created = e => console.log(e);
 
@@ -31,20 +30,28 @@ const MapComponent = ({ state, district }) => {
           setPosition(resultPosition);
         }
       } catch (error) {
+        console.error(error);
       }
     };
 
-    if (state||district) {
+    if (state || district) {
       fetchLocation();
     }
   }, [state, district]);
 
   return (
-    <MapContainer  center={position} zoom={10} style={{ height: "500px", width: "100%" }} ref={mapRef}>
+    <MapContainer center={position} zoom={10} style={{ height: "500px", width: "100%" }} ref={mapRef}>
       <FeatureGroup>
-        <EditControl position="topright" onCreated={_created} draw={{rectangle: true, polyline: true, polygon: true, circle: true, marker: true, circlemarker: true}}/>
+        <EditControl
+          position="topright"
+          onCreated={_created}
+          draw={{ rectangle: true, polyline: true, polygon: true, circle: true, marker: true, circlemarker: true }}
+        />
       </FeatureGroup>
-      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'/ >
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
       <UpdateMapPosition position={position} />
     </MapContainer>
   );

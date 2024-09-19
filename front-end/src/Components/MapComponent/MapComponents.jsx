574ee8345f72b3c68c03b4070d9d61be9cbc6ee2
Nandit <nanditsharma063@gmail.com>
2024-09-19 12:28:48 +0530

    updated Map Component

--- front-end/src/Components/MapComponent/MapComponents.jsx
+++ front-end/src/Components/MapComponent/MapComponents.jsx
@@ -1,12 +1,17 @@
-import React, { useState, useEffect } from "react";
-import { MapContainer, TileLayer } from "react-leaflet";
+import React, { useState, useEffect, useRef } from "react";
+import { MapContainer, TileLayer, FeatureGroup } from "react-leaflet";
+import { EditControl } from "react-leaflet-draw";
 import "leaflet/dist/leaflet.css";
+import "leaflet-draw/dist/leaflet.draw.css";
 import sendLocation from "../../APIMiddleware/APIMiddleware.js";
 import UpdateMapPosition from "../../Utils/UpdateMapPosition.js";
 import "./MapComponent.css";
 
 const MapComponent = ({ state, district }) => {
   const [position, setPosition] = useState([23.5000, 80.7500]);
+  const mapRef = useRef()
+
+  const _created = e => console.log(e);
 
   useEffect(() => {
     const fetchLocation = async () => {
@@ -25,7 +30,10 @@ const MapComponent = ({ state, district }) => {
   }, [state, district]);
 
   return (
-    <MapContainer  center={position} zoom={10} style={{ height: "500px", width: "100%" }}>
+    <MapContainer  center={position} zoom={10} style={{ height: "500px", width: "100%" }} ref={mapRef}>
+      <FeatureGroup>
+        <EditControl position="topright" onCreated={_created} draw={{rectangle: true, polyline: true, polygon: true, circle: true, marker: true, circlemarker: true}}/>
+      </FeatureGroup>
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'/ >
       <UpdateMapPosition position={position} />
     </MapContainer>
